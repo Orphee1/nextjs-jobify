444e6d49d4826853a312507c02f74765baceb6d8
Hugo Lattard <hugolattard@gmail.com>
2025-02-21 17:45:46 +0100

    Added first pages

---
+++ app/(dashboard)/add-job/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function AddJobPage() {
+  return <h1 className='text-7xl'>Add Job Page</h1>
+}
---
+++ app/(dashboard)/stats/page.tsx
@@ -0,0 +1,3 @@
+export default function StatsPage() {
+  return <h1 className='text-7xl'>Stats Page</h1>
+}
